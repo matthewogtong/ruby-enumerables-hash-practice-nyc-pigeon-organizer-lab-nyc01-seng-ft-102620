6ea9d3073bde37ae5fb91fda130c167f439f64e6
Matthew Ogtong <ogtongm@gmail.com>
2020-10-14 20:05:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,6 +2,7 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
 
+<<<<<<< HEAD
   final_results = data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, names|
       names.each do |name|
@@ -18,4 +19,14 @@ def nyc_pigeon_organizer(data)
     # final_array
   end
   # binding.pry
+=======
+  data.each_with_object({}) do |(key, value), final_array|
+    value.each do |inner_key, names|
+      names.each do |name|
+      end
+      binding.pry
+    end
+    final_array
+  end
+>>>>>>> 3647c671e866e558ba1e2354e056ae959e527800
 end
